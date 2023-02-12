f6b56baee42810314f3f708f4bba733a965d8768
waldirdev01 <waldirdev01@gmail.com>
2023-02-11 22:26:58 -0300

    Buscando usuários no Firebase

--- lib/models/admin_user_manager/admin_user_manager.dart
+++ lib/models/admin_user_manager/admin_user_manager.dart
@@ -1,35 +1,52 @@
-
-import 'package:faker/faker.dart';
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:quem_ama_da_doce/models/app_user/app_user.dart';
 
 import '../app_user/app_user_manager.dart';
 
-class AdminUsersManager extends ChangeNotifier{
-
+class AdminUsersManager extends ChangeNotifier {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   List<AppUser> users = [];
+  StreamSubscription? _subscription;
 
-  void updateUser(UserManager userManager){
-    if(userManager.adminEnabled){
+  void updateUser(UserManager userManager) {
+    //a caso o usuário não seja admin, cancela o Stream, se for nulo não chama
+    _subscription?.cancel();
+    if (userManager.adminEnabled) {
       _listenToUsers();
+    } else {
+      //para reduzir o risco de um usuário comum ver a lista de usuários
+      users.clear();
+      notifyListeners();
     }
   }
 
-  void _listenToUsers(){
-    final faker = Faker();
+  void _listenToUsers() {
+    //busca a lista uma vez quando acessa o app
+    /*firestore.collection('users').get().then((snapshot) {
+      users = snapshot.docs.map((e) => AppUser.fromDocument(e)).toList();
+      users
+          .sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
 
-    for(int i = 0; i < 1000; i++){
-      users.add(AppUser(
-          name: faker.person.name(),
-          email: faker.internet.email()
-      ));
-    }
+      notifyListeners();
+    });*/
+    //escuta as alterações na lista instantaneamente, mas fica ouvindo o constantemente
+    //O StreamSubscription é utilizado pelo dispose() para matar essa chamada
+    firestore.collection('users').snapshots().listen((snapshot) {
+      users = snapshot.docs.map((e) => AppUser.fromDocument(e)).toList();
+      users
+          .sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
 
-    users.sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+      notifyListeners();
+    });
+  }
 
-    notifyListeners();
+  @override
+  dispose() {
+    _subscription?.cancel();
+    super.dispose();
   }
 
   List<String> get names => users.map((e) => e.name).toList();
-
 }
--- lib/screens/admin_screens/admins_screen.dart
+++ lib/screens/admin_screens/admins_screen.dart
@@ -2,6 +2,7 @@
 import 'package:alphabet_list_scroll_view/alphabet_list_scroll_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:quem_ama_da_doce/common/custom_drawer/custom_drawer.dart';
 
 import '../../models/admin_user_manager/admin_user_manager.dart';
 
@@ -9,6 +10,7 @@ class AdminUsersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const CustomDrawer(),
       appBar: AppBar(
         title: const Text('Usuários'),
         centerTitle: true,
